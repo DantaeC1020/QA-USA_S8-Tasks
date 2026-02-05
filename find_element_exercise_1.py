bb973e1640714debef6ddff8c4d12137c4279d87
DantaeC <carlton.dantae@gmail.com>
2026-02-04 19:44:21 -0500

    learning

--- find_element_exercise_1.py
+++ find_element_exercise_1.py
@@ -7,13 +7,13 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 
 # Open the specified URL in the browser
-driver.get(" SERVER URL ")
+driver.get("https://cnt-80ac471e-ea60-4e66-9925-c1c2e6385c8a.containerhub.tripleten-services.com")
 
 # Pause execution for 2 seconds to allow the page to load fully
 time.sleep(2)
 
 # Find the title element using its CSS Selector
-driver...
+driver.find_element(By.CSS_SELECTOR, ".logo-disclaimer")
 
 # Close the browser and end the WebDriver session
-driver...
+driver.quit()
--- open_and_close_page.py
+++ open_and_close_page.py
@@ -4,7 +4,7 @@ driver = webdriver.Chrome()
 driver.maximize_window()
 
 # Open the Urban Routes home page
-driver.get("")
+driver.get("https://cnt-31181238-0c08-45ab-ac57-488ae3f11916.containerhub.tripleten-services.com")
 
 # Check url contains tripleten-services.com
 assert 'tripleten-services.com' in driver.current_url
